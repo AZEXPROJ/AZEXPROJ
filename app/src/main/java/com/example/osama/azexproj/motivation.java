f79b2fb9e5d26c39cafd6a32febbefeaf7f2af77
Omar Al-Sorogy <omaralsorogy@gmail.com>
2018-04-04 00:35:08 +0200

    change motivation

--- app/src/main/java/com/example/osama/azexproj/motivation.java
+++ app/src/main/java/com/example/osama/azexproj/motivation.java
@@ -1,51 +1,42 @@
 package com.example.osama.azexproj;
-
+import android.content.Context;
 import android.content.Intent;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
 public class motivation extends AppCompatActivity {
 ImageView video ;
 ImageView stories;
 ImageView qoutes;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_motivation);
-
         video = (ImageView) findViewById(R.id.video);
-
-
-
-
         video.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( motivation.this ,videos.class);
+                Intent intent = new Intent( context ,videos.class);
                 startActivity(intent);
-
             }
         });
-
         stories = (ImageView) findViewById(R.id.stories);
         stories.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( motivation.this ,stories.class);
+                Intent intent = new Intent( context ,stories.class);
                 startActivity(intent);
-
             }
         });
         qoutes = (ImageView) findViewById(R.id.qoutes);
         qoutes.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent( motivation.this ,quotes.class);
+                Intent intent = new Intent( context ,quotes.class);
                 startActivity(intent);
-
             }
         });
     }
