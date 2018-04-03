496f7ea2b74b7a186c66581047f95a2a97a84a13
AbdElrahmaN31 <abdelrahmannashaat0@gmail.com>
2018-04-04 01:43:18 +0200

    Remove errors in videos activity

--- app/src/main/java/com/example/osama/azexproj/videos.java
+++ app/src/main/java/com/example/osama/azexproj/videos.java
@@ -10,28 +10,28 @@ import android.webkit.WebView;
 import android.widget.ListView;
 public class videos extends AppCompatActivity {
     Context context;
-    public View getView(int arg0, View convertView, ViewGroup arg2) {
-        // TODO Auto-generated method stub
-
-        LayoutInflater inflater =  context.getLayoutInflater();
-        convertView = inflater.inflate(R.layout.listitem, null);
-
-        WebView wv = (WebView)convertView.findViewById(R.id.webview);
-        wv.getSettings().setJavaScriptEnabled(true);
-        wv.loadUrl("http://www.google.com");
-
-        convertView.setTag(wv);
-
-        return convertView;
-    }
+//    public View getView(int arg0, View convertView, ViewGroup arg2) {
+//        // TODO Auto-generated method stub
+//
+//        LayoutInflater inflater =  context.getLayoutInflater();
+//        convertView = inflater.inflate(R.layout.listitem, null);
+//
+//        WebView wv = (WebView)convertView.findViewById(R.id.webview);
+//        wv.getSettings().setJavaScriptEnabled(true);
+//        wv.loadUrl("http://www.google.com");
+//
+//        convertView.setTag(wv);
+//
+//        return convertView;
+//    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_videos);
         ListView lv = (ListView)findViewById(R.id.listview);
-        ListViewAdapter adapter = new ListViewAdapter(this);
-
-        lv.setAdapter(adapter);
+//        ListViewAdapter adapter = new ListViewAdapter(this);
+//
+//        lv.setAdapter(adapter);
 
 
 
